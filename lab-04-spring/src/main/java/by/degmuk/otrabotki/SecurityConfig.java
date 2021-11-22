8a4cea322f701a4c19e9a8ff3d0c212bd8bfa8af
dgmk <degmuk@gmail.com>
2021-11-22 11:33:40 +0300

    Default user in application.properties

--- lab-04-spring/src/main/java/by/degmuk/otrabotki/SecurityConfig.java
+++ lab-04-spring/src/main/java/by/degmuk/otrabotki/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -23,15 +22,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and().logout().permitAll()
 				.disable().cors();
 		http.csrf().disable();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(userDetails);
 	}
 }
